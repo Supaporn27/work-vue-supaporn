fa2312bc5ced1fd22046e3393defeda97ee83b5c
USER <EMAIL>
2021-11-18 09:13:33 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <div class="bg text-center">
+      <span class="white--text">
+        <v-container>
+          <Nuxt />
+        </v-container>
+      </span>
+    </div>
+  </v-app>
+</template>
+
+// แบบเขียนcss
+<style>
+.bg {
+  /* background-color: #0989ff; */
+  background: linear-gradient(to top right,#5842ff,#0989ff,#038fff);
+}
+</style>
